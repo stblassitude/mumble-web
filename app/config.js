f18328f0e3d22d73e56a771e41bf3982527a6773
Oskar <oskar@winkels.berlin>
2019-12-25 10:25:17 +0100

    reconfigre addresses and such for deployment

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ window.mumbleWebConfig = {
   // Default values (can be changed by passing a query parameter of the same name)
   'defaults': {
     'username': ('c3lingo-web-' + Math.random().toString(36).substring(6)),
-    'address': 'c3lingo.winkels.berlin', // window.location.hostname,
-    'port': '64737'
+    'address': 'streaming.c3lingo.org/mumble', // window.location.hostname,
+    'port': '443'
   }
 }
